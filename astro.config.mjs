93b5075c42dd1537c5829efd6167cf5b3294f985
Pol Gubau <gubaupol@gmail.com>
2023-11-13 20:16:14 +0100

    Remove Svelte integration and Greeting component, update Astro integrations.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
 import react from "@astrojs/react";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte(), react()],
+  integrations: [tailwind(), react()],
   output: "server",
   adapter: vercel()
 });
